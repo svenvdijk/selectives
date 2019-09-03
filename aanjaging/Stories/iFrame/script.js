2e9a8c4b32a05babc246cd3aefa49f1a04c317bb
Sven van Dijk <s.vandijk@persgroep.nl>
2019-09-03 09:33:42 +0200

    Mitchell zijn aanpassingen zijn toegevoegd aan de style sheet. Hier en daar wat aanpassingen gemaakt om het geschikt te maken voor live gang.

--- aanjaging/Stories/iFrame/script.js
+++ aanjaging/Stories/iFrame/script.js
@@ -1,11 +1,12 @@
-    var windowWidth = $(window.top).width(); 
+
     $.fn.aspectRatio = function(event) {
+        var windowWidth = $(window.top).width(); 
         var padding = 40;
         var width = windowWidth - padding;
         var ratioWidth = 125;
         var ratioHeight = 224;
         var ratio = width / ratioWidth;
         var height = ratio * ratioHeight;
-        
+
     }
     $('.image-wrapper').aspectRatio();
